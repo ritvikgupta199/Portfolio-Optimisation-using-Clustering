6b4f30e11db06a47e05fd7444fe5443291af4dc9
Anshuman Panda <anshumanpanda211@gmail.com>
2022-04-14 17:06:43 +0530

    quarterly prices compiled

--- data/compile_prices.py
+++ data/compile_prices.py
@@ -1,4 +1,5 @@
 import json
+from math import ceil
 
 syms = []
 dates = []
@@ -26,15 +27,31 @@ f.close()
 for key, val in data["Time Series (Daily)"].items():
     dates.append(key)
 
-f = open('prices.csv','w')
+f = open('data/prices/prices.csv','w')
 f.write('Date,')
 for sym in syms:
     f.write(sym+',')
 f.write('\n')
+for year in range(1999, 2023):
+    for qtr in range(1, 5):
+        f_ = open('data/prices/prices_'+str(year)+'q'+str(qtr)+'.csv','w')
+        f_.write('Date,')
+        for sym in syms:
+            f_.write(sym+',')
+        f_.write('\n')
+# print(dates)
 for i in range(len(dates)):
+    year = int(dates[i][0:4])
+    qtr = int(dates[i][5:7])
+    qtr = ceil(qtr/3)
+    # print(year, qtr)
+    f_ = open('data/prices/prices_'+str(year)+'q'+str(qtr)+'.csv','a')
     f.write(dates[i]+',')
+    f_.write(dates[i]+',')
     for j in range(len(prices)):
         if i >= len(prices[j]):
             prices[j].append("")
         f.write(str(prices[j][i])+',')
+        f_.write(str(prices[j][i])+',')
     f.write('\n')
+    f_.write('\n')
