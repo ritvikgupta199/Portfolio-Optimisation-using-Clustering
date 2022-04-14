0298e67c9755191b75bde82b5e8eb7ef2eee6f02
Ritvik Gupta <ritvikgupta199@gmail.com>
2022-04-14 16:51:28 +0530

    Added script

--- MeanVariance/meanvar.py
+++ MeanVariance/meanvar.py
@@ -7,6 +7,7 @@ from pypfopt import discrete_allocation
 from pypfopt import risk_models
 from pypfopt import expected_returns
 
+
 PRICES = '../prices.csv'
 TICKER_FILE = 'tickers/tickers.csv'
 tickers = [ticker.strip() for ticker in open(TICKER_FILE, 'r').readlines()]
@@ -14,5 +15,19 @@ tickers = [ticker.strip() for ticker in open(TICKER_FILE, 'r').readlines()]
 prices_df = pd.read_csv(PRICES)
 prices_df = prices_df.set_index('Date')
 prices_df = prices_df[tickers]
-print(prices_df.head())
+
+mu = expected_returns.mean_historical_return(prices_df)
+sigma = risk_models.sample_cov(prices_df)
+
+ef = EfficientFrontier(mu, sigma, weight_bounds=(0,1))
+sharpe_pfolio=ef.min_volatility()
+sharpe_pwt=ef.clean_weights()
+print(f'The weights for min volatility are {sharpe_pwt}')
+
+latest_prices = discrete_allocation.get_latest_prices(prices_df)
+allocation_minv, rem_minv = discrete_allocation.DiscreteAllocation(sharpe_pwt, latest_prices, total_portfolio_value=10000).lp_portfolio()
+
+print('The allocation is as follows:')
+for key, val in allocation_minv.items():
+    print(f'{key}, {val}')
 
