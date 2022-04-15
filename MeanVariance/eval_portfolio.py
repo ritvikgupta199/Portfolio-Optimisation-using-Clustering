3a7af4668ac1dad300807d92f20f4af0630df16c
Ritvik Gupta <ritvikgupta199@gmail.com>
2022-04-15 17:36:04 +0530

    Added Mean var for all

--- MeanVariance/eval_portfolio.py
+++ MeanVariance/eval_portfolio.py
@@ -6,45 +6,61 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 
+def get_tickers_wts(filename):
+    tickers = []
+    pf_wts = {}
+    fr = open(filename, 'r')
+    for line in fr.readlines():
+        tokens = line.split(',')
+        tickers.append(tokens[0].strip())
+        pf_wts[tokens[0]] = float(tokens[1])
+    return tickers, pf_wts
+
+def get_prices_alloc(pf_wts, tickers, prices):
+    prices_pf = prices[tickers]
+    latest_prices = prices_pf.loc[prices_pf.index.min()]
+    pf_alloc = {}
+    for idx, wt in pf_wts.items():
+        pf_alloc[idx] = (wt * PF_AMOUNT) / latest_prices[idx]
+    return prices_pf, pf_alloc
+
+def get_value(prices, tickers, pf_alloc):
+    val = 0
+    for ticker in tickers:
+        val += prices[ticker] * pf_alloc[ticker]
+    return val
+
 PF_AMOUNT = 10000
 EVAL_PRICES = '../data/prices/prices_2021q2.csv'
-PORTFOLIO = 'portfolios/portfolio.csv'
-tickers = []
-pf_wts = {}
-fr = open(PORTFOLIO, 'r')
-for line in fr.readlines():
-    tokens = line.split(',')
-    tickers.append(tokens[0].strip())
-    pf_wts[tokens[0]] = float(tokens[1])
+PORTFOLIO_BM = 'portfolios/portfolio_bm.csv'
+PORTFOLIO_ALL = 'portfolios/portfolio_all.csv'
+
+tickers_bm, bm_wts = get_tickers_wts(PORTFOLIO_BM)
+tickers_all, all_wts = get_tickers_wts(PORTFOLIO_ALL)
 
 prices_df = pd.read_csv(EVAL_PRICES)
 prices_df = prices_df.set_index('Date')
 prices_df = prices_df.sort_index(ascending=True)
-prices_df = prices_df[tickers]
 
-latest_prices = prices_df.loc[prices_df.index.min()]
-pf_alloc = {}
-for idx, wt in pf_wts.items():
-    pf_alloc[idx] = (wt * PF_AMOUNT) / latest_prices[idx]
+prices_bm, bm_alloc = get_prices_alloc(bm_wts, tickers_bm, prices_df)
+prices_all, all_alloc = get_prices_alloc(all_wts, tickers_all, prices_df)
 
 sp500_df = web.DataReader('sp500', start = prices_df.index.min(), end = prices_df.index.max(), data_source='fred')
 sp500_qt = PF_AMOUNT / sp500_df.loc[prices_df.index.min()]['sp500']
 
-dates, val_pf, val_sp = [], [], []
+dates, val_bm, val_all, val_sp = [], [], [], []
 for idx, row in prices_df.iterrows():
     dates.append(idx)
-    val = 0
-    for ticker in tickers:
-        val += row[ticker] * pf_alloc[ticker]
-    val_pf.append(val)
+    val_bm.append(get_value(row, tickers_bm, bm_alloc))
+    val_all.append(get_value(row, tickers_all, all_alloc))
     val_sp.append(sp500_df.loc[idx]['sp500']*sp500_qt)
-
 
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 ax.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
 
-ax.plot(dates, val_pf, color='red', label='Portfolio')
+ax.plot(dates, val_bm, color='green', label='Portfolio using BM')
+ax.plot(dates, val_all, color='red', label='Portfolio using All')
 ax.plot(dates, val_sp, color='blue', label='S&P500 Index')
 
 ax.set_xlabel("Date")
--- MeanVariance/meanvar.py
+++ MeanVariance/meanvar.py
@@ -9,31 +9,41 @@ from pypfopt import risk_models
 from pypfopt import expected_returns
 
 
+def get_mean_var_wts(prices):
+    mu = expected_returns.mean_historical_return(prices)
+    sigma = risk_models.sample_cov(prices)
+    ef = EfficientFrontier(mu, sigma, weight_bounds=(0,1))
+    portfolio = ef.min_volatility()
+    pf_wt = ef.clean_weights()
+    return pf_wt
+
+def write_wts(filename, wts):
+    print(f'The weights for {filename} are as follows:')
+    fw = open(filename, 'w')
+    for idx, wt in wts.items():
+        print(f'{idx}, {wt}')
+        fw.write(f'{idx}, {wt}\n')
+    print('\n')
+    fw.close()
+
 PF_AMOUNT = 10000
 PRICES = '../data/prices/prices_2021q1.csv'
 TICKER_FILE = '../bm_landmarks'
 SNP_LIST = '../snp500sym.csv'
-PORTFOLIO = 'portfolios/portfolio.csv'
+PORTFOLIO_BM = 'portfolios/portfolio_bm.csv'
+PORTFOLIO_ALL = 'portfolios/portfolio_all.csv'
 
 snp_list = open(SNP_LIST, 'r').readlines()
-tickers = [snp_list[int(ticker)-1].strip() for ticker in np.arange(201, 301)]
-# print(tickers)
-# tickers = [snp_list[int(ticker)-1].strip() for ticker in open(TICKER_FILE, 'r').readlines()]
+tickers_all = [snp_list[int(ticker)].strip() for ticker in np.arange(200, 300)]
+tickers_bm = [snp_list[int(ticker)-1].strip() for ticker in open(TICKER_FILE, 'r').readlines()]
 
 prices_df = pd.read_csv(PRICES)
 prices_df = prices_df.set_index('Date')
-prices_df = prices_df[tickers]
 
-mu = expected_returns.mean_historical_return(prices_df)
-sigma = risk_models.sample_cov(prices_df)
+prices_bm = prices_df[tickers_bm]
+bm_wts = get_mean_var_wts(prices_bm)
+write_wts(PORTFOLIO_BM, bm_wts)
 
-ef = EfficientFrontier(mu, sigma, weight_bounds=(0,1))
-sharpe_pfolio=ef.min_volatility()
-sharpe_pwt=ef.clean_weights()
-
-print('The weights are as follows:')
-fw = open(PORTFOLIO, 'w')
-for idx, wt in sharpe_pwt.items():
-    print(f'{idx}, {wt}')
-    fw.write(f'{idx}, {wt}\n')
-
+prices_all = prices_df[tickers_all]
+all_wts = get_mean_var_wts(prices_all)
+write_wts(PORTFOLIO_ALL, all_wts)
